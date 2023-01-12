07faaefb1bf7809dfbf334fcda4de9ca7e7357e8
AlbiGit <AlbertKornilov2022@gmail.com>
2023-01-12 13:29:01 +0200

    Changes

--- Gamecards/Game.py
+++ Gamecards/Game.py
@@ -5,15 +5,15 @@ from Gamecards import Card_Class
 player1 = f'jimmy'
 player2 = f'dudi'
 war = CardGame_Class.CardGame(player1,player2,26)
-print(f'players are {player1},{player2} with 26 cards each')
+print(f'players are {player1} and {player2} with 26 cards each')
 for i in range(3):
-    card1 = Player_Class.Player.player1.get_card()
-    card2 = Player_Class.Player.player2.get_card()
+    card1 = war.player_a.get_card()
+    card2 = war.player_b.get_card()
     if card1 > card2:
-        player1.Player_Class.Player.add_card(card1)
-        player1.Player_Class.Player.add_card(card2)
-        print(player1,f'wins and get {card1} and {card2}')
+        war.player_a.add_card(card1)
+        war.player_a.add_card(card2)
+        print(f'winnter is {war.player_a.player_name} and gets {card1} and {card2}')
     else:
-        player2.Player_Class.Player.add_card(card1)
-        player2.Player_Class.Player.add_card(card2)
-        print(player2, f'wins and get {card1} and {card2}')
+        war.player_b.add_card(card1)
+        war.player_b.add_card(card2)
+        print(f'winnter is {war.player_b.player_name} and gets {card1} and {card2}')
