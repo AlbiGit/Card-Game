d5302db2aa313b28ee1aeb494f67a5ce493e21e7
Avivrogo <avivrogo34@gmail.com>
2023-01-12 12:13:53 +0200

    Changes

--- Gamecards/Game.py
+++ Gamecards/Game.py
@@ -1,3 +1,17 @@
-import Gamecards
-Gamecards.CardGame_Class.CardGame("fsasd","ads", 10)
-print()
+from Gamecards import CardGame_Class
+from Gamecards import Player_Class
+from Gamecards import Card_Class
+
+player1 = f'jimmy'
+player2 = f'dudi'
+war = CardGame_Class.CardGame(player1,player2,26)
+print(f'players are {player1},{player2} with 26 cards each')
+for i in range(3):
+    card1 = player1.Player_Class.Player.get_card()
+    card2 = player2.Player_Class.Player.get_card()
+    if card1 > card2:
+        player1.Player_Class.Player.add_card(card1)
+        player1.Player_Class.Player.add_card(card2)
+    else:
+        player2.Player_Class.Player.add_card(card1)
+        player2.Player_Class.Player.add_card(card2)
--- Gamecards/Player_Class.py
+++ Gamecards/Player_Class.py
@@ -3,6 +3,7 @@ from DeckOfCards_Class import DeckOfCards
 import random
 class Player:
     def __init__(self, name:str, number_of_cards_to_deal: int):
+        """sets a new player"""
         if not 10 <= number_of_cards_to_deal <= 26:
             number_of_cards_to_deal = 26
         self.player_name = name
@@ -10,13 +11,16 @@ class Player:
         self.cards_amount = number_of_cards_to_deal
 
     def set_hand(self, deck: DeckOfCards):
+        """sets the player's hand according to the amount of cards entered at the beginning of the game"""
         for i in range(self.cards_amount):
             card = deck.deal_one()
             self.player_hand.append(card)
 
     def get_card(self):
+        """takes one card out of the player's hand and returns it"""
         card = self.player_hand.pop(random.randint(0, len(self.player_hand) - 1))
         return card
 
     def add_card(self, card: Card):
+        """adds a card to the player's hand"""
         self.player_hand.append(card)
