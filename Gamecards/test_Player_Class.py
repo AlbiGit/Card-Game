f9512b0bd897462bd11ba6d1f08f640ec3b681bb
Avivrogo <avivrogo34@gmail.com>
2023-01-12 19:05:07 +0200

    player test

---
+++ Gamecards/test_Player_Class.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from Card_Class import Card
+from DeckOfCards_Class import DeckOfCards
+import random
+
+class TestPlayer(TestCase):
+    def test_set_hand(self):
+        self.fail()
+
+    def test_get_card(self):
+        self.fail()
+
+    def test_add_card(self):
+        self.fail()
